3e00308d3bfb9b8d2841dc31a743c1c05dc3dfd0
Thiago11112000 <thiagoclick2000@hotmail.com>
2024-04-26 17:08:28 -0300

    Inicializando projeto React, jsx e sem jsx

---
+++ 02jsx/src/index.js
@@ -0,0 +1,10 @@
+// Importar módulos
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+// Sem JSX
+// const elenmento =  React.createElement('h1',{},'sem jsx');
+// ReactDom.render(elenmento, document.getElementById('root'))
+ReactDOM.render(<h1> COM  JSX</h1>, document.getElementById('root') )
