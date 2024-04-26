804cffba02dfb361b3c9fc57c0c911ef59388b5f
Thiago11112000 <thiagoclick2000@hotmail.com>
2024-04-26 17:18:09 -0300

    Componente

---
+++ 03componente/src/index.js
@@ -0,0 +1,17 @@
+// Importar Módulos
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Componente
+
+class MeuComponente extends React.Component{
+    render(){
+        return <h1> Meu primeiro componente</h1>
+    }
+}
+
+// Render
+ReactDOM.render(<MeuComponente/> , document.getElementById('root'))
+
+
